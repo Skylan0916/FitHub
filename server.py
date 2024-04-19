74790872ebbfab75f6a54a0a6cede4634bfd9f9a
Peter Lan <skylan@Peters-MacBook.local>
2024-04-19 02:30:41 -0700

    Add answer selection and feedback feature

--- server.py
+++ server.py
@@ -1,34 +1,39 @@
 from flask import Flask
 from flask import render_template
-from flask import Response, request, jsonify
+from flask import Response, request
 
 from data.learn import muscle_groups, exercises
 from data.quiz import quizzes
 
 app = Flask(__name__)
 
+score = 0
+quiz_index = 0
 completed_dict = {group["name"]: False for group in muscle_groups}
-current_quiz_index = 0
+
+
+def reset():
+    global score, quiz_index, completed_dict
+    score = 0
+    quiz_index = 0
+    completed_dict = {group["name"]: False for group in muscle_groups}
 
 
 @app.route('/')
 def home():
-    return render_template('home.html') 
+    reset()
+    return render_template('home.html')
 
 
-@app.route('/learn', methods=['GET', 'POST'])
+@app.route('/learn', methods=['GET'])
 def learn():
-    completed_section = request.form.get("completed_section")
-    if completed_section:
-        completed_dict[completed_section] = True
-
-    n_completed = sum(1 for value in completed_dict.values() if value)
     total = len(completed_dict)
+    n_completed = sum(1 for value in completed_dict.values() if value)
 
     return render_template('learn.html', muscle_groups=muscle_groups, n_completed=n_completed, total=total)
 
 
-@app.route('/learn/<muscle_group>/<index>')
+@app.route('/learn/<muscle_group>/<index>', methods=['GET'])
 def learn_exercise(muscle_group, index):
     index = int(index)
     total = len(exercises[muscle_group])
@@ -37,13 +42,36 @@ def learn_exercise(muscle_group, index):
     return render_template('exercise.html', exercise=exercise, muscle_group=muscle_group, current_index=index, total=total)
 
 
-@app.route('/quiz/<index>', methods=['GET', 'POST'])
-def quiz(index):
-    index = int(index)
-    quiz = quizzes[index]
+@app.route('/complete', methods=['POST'])
+def complete_section():
+    section = request.form.get("section")
+    completed_dict[section] = True
+
+    return Response(status=204)
+
+
+@app.route('/quiz', methods=['GET'])
+def quiz():
+    quiz = quizzes[quiz_index]
     total = len(quizzes)
 
-    return render_template('quiz.html', quiz=quiz, current_index=index, total=total) 
+    if quiz_index >= total - 1:
+        return render_template('result.html', score=score)
+
+    return render_template('quiz.html', quiz=quiz, current_index=quiz_index, total=total)
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    global score, quiz_index
+    
+    quiz_index += 1
+
+    scored = request.form.get("scored") == "true"
+    if scored:
+        score += 1
+
+    return Response(status=204)
 
 
 if __name__ == '__main__':
---
+++ static/quiz.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    let selectedChoices = [];
+
+    // Handle choice selection
+    $(document).on('click', '.choice', function () {
+        const id = $(this).data('id');
+
+        if (selectedChoices.includes(id)) {
+            selectedChoices = selectedChoices.filter(x => x !== id);
+        } else {
+            selectedChoices.push(id);
+        }
+    });
+
+    // Handle quiz submission
+    $(document).on('click', '.submit_btn', function () {
+        if (selectedChoices.length === 0) {
+            alert('Please select an answer.');
+            return;
+        }
+
+        const solutions = $(this).data('solutions');
+        const isCorrect = selectedChoices.every(choiceId => {
+            return solutions.includes(choiceId);
+        });
+        const scored = (isCorrect && selectedChoices.length === solutions.length);
+
+        // Display Feedback
+        if (scored) {
+            alert('Your answer is correct!');
+        } else {
+            alert('Your answer is incorrect!');
+        }
+
+        $.ajax({
+            type: 'POST',
+            url: "/submit",
+            data: { scored: scored },
+            success: function(response) {
+                window.location.href = "/quiz";
+            },
+            error: function(xhr, status, error) {
+                console.error(xhr.responseText);
+            }
+        });
+    });
+
+})
