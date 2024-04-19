1b0a75a22b6cf2f59455e1725dafc58472c24520
Peter Lan <skylan@Peters-MacBook.local>
2024-04-19 02:30:14 -0700

    Refactor

--- static/learn.js
+++ static/learn.js
@@ -1,21 +1,20 @@
 $(document).ready(function(){
 
-    // Selecting a specific learning section
+    // Handle learning section selection
     $(document).on('click', '.muscle_group', function() {
         const id = $(this).data('id');
         window.location.href = `/learn/${id}/0`;
     });
 
-    // Completion of a learning section
+    // Handle learning section completion
     $('.complete_btn').on('click', function() {
         const section = $(this).data('id');
 
         $.ajax({
             type: 'POST',
-            url: '/learn',
-            data: { completed_section: section },
+            url: '/complete',
+            data: { section: section },
             success: function(response) {
-                // console.log(section)
                 window.location.href = '/learn';
             },
             error: function(xhr, status, error) {
