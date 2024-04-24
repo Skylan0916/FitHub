2265cfb27af465997ee73d935f8c6048eaeeeb39
Peter Lan <skylan@Peters-MacBook.local>
2024-04-23 20:21:23 -0700

    Refactor

--- static/learn.js
+++ static/learn.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
 
     // Handle learning section selection
-    $(document).on('click', '.muscle_group', function() {
+    $(document).on('click', '.muscle-group', function() {
         const id = $(this).data('id');
         window.location.href = `/learn/${id}/0`;
     });
 
     // Handle learning section completion
-    $('.complete_btn').on('click', function() {
+    $('.complete-btn').on('click', function() {
         const section = $(this).data('id');
 
         $.ajax({
